2bf67ac1d905c0bb2d27f7f0d4b0c3ab66076f29
Manuel <elmer.manuel00@gmail.com>
2025-06-26 01:52:08 +0200

    added typing to pinia store

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia'
+import type {User} from '~/interfaces/auth/user'
 
 export const useUserStore = defineStore('user', () => {
-  const user = ref({});
+  const user = ref<User>();
   const jwt: Ref<string> = ref('');
   const refreshToken: Ref<string> = ref('');
 
