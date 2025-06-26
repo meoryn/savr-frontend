f5aa7d8f1489e5010c696b856b689b9d938d7a0c
Manuel <elmer.manuel00@gmail.com>
2025-06-26 23:04:17 +0200

    added password

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,18 @@
 <template>
     <UContainer class="p-5 flex flex-col gap-4">
         <h1 class="text-4xl font-bold text-center">Profil anpassen</h1>
+        <h3 class="text-xl font-bold">Passwort ändern</h3>
+        <UForm :schema="passwordSchema" :state="passwordState" class="flex flex-col gap-4" @submit="changePassword">
+            <UFormField label="newPassword" name="newPassword">
+                <UInput v-model="passwordState.newPassword" type="password" class="w-full"/>
+            </UFormField>
+            <UFormField label="repeatPassword" name="repeatPassword">
+                <UInput v-model="passwordState.repeatPassword" type="password" class="w-full"/>
+            </UFormField>
+
+            <UButton type="submit"> Password ändern </UButton>
+        </UForm>
+        <hr>
         <UForm :schema="schema" :state="state" class="flex flex-col gap-4" @submit="adjustProfile">
             <UFormField label="Username" name="username">
                 <UInput v-model="state.username" type="text" class="w-full" />
@@ -11,19 +23,19 @@
             <UFormField label="Lastname" name="last_name">
                 <UInput v-model="state.lastName" type="text" class="w-full" />
             </UFormField>
+
             <UButton type="submit"> Profil anpassen </UButton>
         </UForm>
     </UContainer>
 </template>
 
 <script setup lang="ts">
-import type { Database } from '~/types/supabase';
 import type { FormSubmitEvent } from '@nuxt/ui'
 import * as z from 'zod';
 
-const user = useSupabaseUser();
-const supabase = useSupabaseClient<Database>();
+const store = useUserStore();
 const toast = useToast();
+const config = useRuntimeConfig();
 
 const schema = z.object({
     username: z.string({
@@ -37,45 +49,100 @@ const schema = z.object({
     }),
 });
 
+const passwordSchema = z.object({
+    newPassword: z.string(),
+    repeatPassword: z.string()
+}).refine(data => data.newPassword == data.repeatPassword,
+    'Passwords must match',
+);
+
+type pSchema = z.output<typeof passwordSchema>;
+
 
 type Schema = z.output<typeof schema>;
 
-const { data: profile } = await supabase
-    .from('profile')
-    .select('*')
-    .eq('id', user!.value!.id)
-    .single();
+    const {data: profile } = await useFetch(`${config.public.apiBaseUrl}/profile`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${store.jwt}`,
+            'x-refresh-token': store.refreshToken,
+        },
+        body: {
+            user_id: store.user?.id,
+        }   
+    });
 
+    console.log(profile);
 
 const state = reactive<Partial<Schema>>({
-    username: profile?.username || '',
-    firstName: profile?.full_name?.split(' ')[0] || '',
-    lastName: profile?.full_name?.split(' ')[1] || '',
+    username: profile?.value.username,
+    firstName: profile?.value.full_name?.split(' ')[0],
+    lastName: profile?.value.full_name?.split(' ')[1],
+});
+
+const passwordState = reactive<Partial<pSchema>>({
+    newPassword: '',
+    repeatPassword: ''
 });
 
 const adjustProfile = async (event: FormSubmitEvent<Schema>) => {
-    console.log('event', event.data);
-    const resp = await supabase
-        .from('profile')
-        .update({
+
+    const resp = await $fetch(`${config.public.apiBaseUrl}/edit_profile`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${store.jwt}`,
+            'x-refresh-token': store.refreshToken,
+        },
+        body: {
+            user_id: store.user?.id,
             username: event.data.username,
             full_name: `${event.data.firstName} ${event.data.lastName}`,
-        })
-        .eq('id', user!.value!.id)
+            
+        },
+    });
 
-    if (resp.error) {
-        toast.add({
-                title: 'Error updating profile',
-                description: resp.error.message,
-                color: 'error', 
-            })
-    } else {
+    if (resp) {
         toast.add({
                 title: 'Success',
                 description: 'Updated profile successfully!',
                 color: 'success', 
             })
+    } else {
+        toast.add({
+                title: 'Error updating profile',
+                description: resp as string,
+                color: 'error', 
+            })
     }
+}
+
+const changePassword = async (event: FormSubmitEvent<pSchema>) => {
+   try {
+     const resp = await $fetch(`${config.public.apiBaseUrl}/change_password`, {
+        method: 'POST',
+        headers: {
+             Authorization: `Bearer ${store.jwt}`,
+            'x-refresh-token': store.refreshToken,
+        },
+        body: {
+            new_password: event.data.newPassword
+        }
+    });
+
+    if(resp) {
+          toast.add({
+                title: 'Success',
+                description: 'Updated password successfully!',
+                color: 'success', 
+            })
+    }
+   } catch (error) {
+     toast.add({
+                title: 'Error updating profile',
+                description: error as string,
+                color: 'error', 
+            })
+   }
 }
  
 </script>
